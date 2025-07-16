02d7cc906ec95ad5115e61248f785029a01db486
Ernest <ernestez12@gmail.com>
2025-07-16 13:48:11 +0100

    ci/cd setup completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
         </Link>
 
         <p>
-          40, floor, Queenslad Victoria Building.
+          20, floor, Queenslad Victoria Building.
           <br /> 60 california street california USA
         </p>
 
